a0f62fe891a900525378ab2a731e45f94aabd417
darshanbhojak18 <43905791+darshanbhojak18@users.noreply.github.com>
2018-10-06 16:21:26 +0530

    Add files via upload

---
+++ deeprem21.py
@@ -0,0 +1,12 @@
+def myadd(num1,num2):
+    sum=num1+num2
+    return sum
+def mysub(num1,num2):
+    sub=num1-num2
+    return sub
+a=int(input("enter a"))
+b=int(input("enter b"))
+c=myadd(a,b)
+d=mysub(a,b)
+print("the sum is",c)
+print("the sub is",d)
